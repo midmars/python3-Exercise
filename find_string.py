7a4ffc1b4e4e85dac586c27933a2d864d129ff90
Ricky Chen <waso1287@gmail.com>
2018-06-06 00:10:39 +0800

    find string test

---
+++ find_string.py
@@ -0,0 +1,31 @@
+def count_substring(string, sub_string):
+    count =0;
+#way1
+    i=0;
+    while i<len(string):
+        if  string.find(sub_string,i) >=0:
+            i=string.find(sub_string,i)+1
+            count+=1;
+        else:
+            break;
+    return count
+#way2
+#     for i in range(0,len(string)-len(sub_string)+1):
+        
+#         if string[i]==sub_string[0]:
+#             flag =1;
+#         for j in range(0,len(sub_string)):
+#             if string[i+j] != sub_string[j]:
+#                 flag =-1;
+#                 break
+#         if flag ==1:
+#             count+=1;
+#     return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
